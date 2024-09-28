3d61437ae29147da7631fd58d6de97789853d66b
root <root@DELL-7559>
2024-09-28 14:21:26 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio17.java
+++ src/main/java/Exercicio17.java
@@ -1,59 +1,55 @@
+// IMPORTS
 
 import java.util.Scanner;
 
 public class Exercicio17 {
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
-	
-		
-		
-		// DECLARATE
-		double a;
-		double b;
-		int option;
-        
-	
-        
-		// MENU
-		System.out.println("======================================");
-		System.out.println("CHOSE AN OPTION");
-		System.out.println("======================================");
-		System.out.println("1 - Engenheiro");
-		System.out.println("2 - Médico");
-		System.out.println("3 - Designer");
-		System.out.println("4 - Programador");
-		System.out.println("5 - Advogado");
-		System.out.print("Your option: ");
-		option = sc.nextInt();
-        
-       
 
-		// OUTPUT
-		switch (option) {
-			case 1:
-				System.out.println("Tibúrcio é engenheiro");
-				break;
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-			case 2:
-				System.out.println("Tibúrcio é médico");
-				break;
+      // DECLARATE
+      double a;
+      double b;
+      int option;
 
-			case 3:
-				System.out.println("Tibúrcio é designer");
-				break;
+      // MENU
+      System.out.println("======================================");
+      System.out.println("CHOSE AN OPTION");
+      System.out.println("======================================");
+      System.out.println("1 - Engenheiro");
+      System.out.println("2 - Médico");
+      System.out.println("3 - Designer");
+      System.out.println("4 - Programador");
+      System.out.println("5 - Advogado");
+      System.out.print("Your option: ");
+      option = sc.nextInt();
 
-			case 4:
-				System.out.println("Tibúrcio é programador");
-				break;
+      // OUTPUT
+      switch (option) {
+         case 1:
+            System.out.println("Tibúrcio é engenheiro");
+            break;
 
-			case 5:
-				System.out.println("Tibúrcio é advogado");
-				break;
+         case 2:
+            System.out.println("Tibúrcio é médico");
+            break;
 
-			default: 
-				System.out.println("Tibúrcio é DESEMPREGADO");
-		}
+         case 3:
+            System.out.println("Tibúrcio é designer");
+            break;
 
-    }
+         case 4:
+            System.out.println("Tibúrcio é programador");
+            break;
+
+         case 5:
+            System.out.println("Tibúrcio é advogado");
+            break;
+
+         default:
+            System.out.println("Tibúrcio é DESEMPREGADO");
+      }
+
+   }
 }
